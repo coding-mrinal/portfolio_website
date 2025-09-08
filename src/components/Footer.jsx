22f8b38a1ab2e010325ab87c84fee8b88ac5ec8d
coding-mrinal <mrinalmahapatra2004@gmail.com>
2025-09-08 10:33:46 +0530

    Included Footer Component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="bg-white dark:bg-gray-800 transition-colors duration-300">
+      <div className="container mx-auto px-4 sm:px-6 text-center">
+        <p className="text-gray-500 dark:text-gray-400 text-sm py-6">
+          © 2025 Mrinal Mahapatra. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
